a56955ae1ce2e987ed68a5e8b89c615fb423778f
unknown <beradifouad@gmail.com>
2020-04-05 21:28:56 +0100

    fixmiddlewarerequestts

--- app/Http/Middleware/FrameHeadersMiddleware.php
+++ app/Http/Middleware/FrameHeadersMiddleware.php
@@ -15,10 +15,9 @@ class FrameHeadersMiddleware
      */
     public function handle($request, Closure $next)
     {
-
+        $response = $next($request);
         $request->header('X-Frame-Options', 'ALLOW FROM https://shopify.com/');
-        //return $response;
-        return $next($request);
+        return $response;
        
     }
 }
