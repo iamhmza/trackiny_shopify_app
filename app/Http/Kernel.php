cb5d126e934785de4853f43454baa9c1c3c87908
unknown <beradifouad@gmail.com>
2020-04-05 22:15:25 +0100

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\FrameHeadersMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -19,7 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\FrameHeadersMiddleware::class,
+        FrameHeadersMiddleware::class,
     ];
 
     /**
