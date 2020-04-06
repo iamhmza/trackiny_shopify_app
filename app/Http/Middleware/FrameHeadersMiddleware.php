f8b0b18ff0b58392ad5aee860201d34d49608d18
unknown <beradifouad@gmail.com>
2020-04-06 02:41:26 +0100

    addingheaderdeny

--- app/Http/Middleware/FrameHeadersMiddleware.php
+++ app/Http/Middleware/FrameHeadersMiddleware.php
@@ -17,7 +17,7 @@ class FrameHeadersMiddleware
     {
 
         $response = $next($request);
-        $response->header('X-Frame-Options', 'SAMEORIGIN');
+        $response->header('X-Frame-Options', 'ALLOW FROM https://trackiny.com/');
         return $response;
        
     }
